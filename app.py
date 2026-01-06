3f466609bcb087ad6bbd7227b1d082703ea921ae
Rahul P <953998190r@gmail.com>
2026-01-06 16:13:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+print("Hello from Jenkins Webhook Demo")
+
+print("Hello from Jenkins Webhook Demo 2")
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print("Addition:", add(2, 3))
